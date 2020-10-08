c52a0d001b8a5a289a1bfcedc8fa38004505c82f
Josué Calderón <39226321+jcalderonv2@users.noreply.github.com>
2020-10-08 01:49:10 -0600

    Create seller list activity

---
+++ app/src/main/java/com/mobile/lotterysmartapp/SellerListActivity.kt
@@ -0,0 +1,14 @@
+package com.mobile.lotterysmartapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerListActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_list)
+    }
+}
