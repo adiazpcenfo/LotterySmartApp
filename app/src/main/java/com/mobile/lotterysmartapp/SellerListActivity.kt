769b0941aa29db94921c04deae273834efee1afb
Josué Calderón <39226321+jcalderonv2@users.noreply.github.com>
2020-10-10 00:43:24 -0600

    Seller list without filters and new style

--- app/src/main/java/com/mobile/lotterysmartapp/SellerListActivity.kt
+++ app/src/main/java/com/mobile/lotterysmartapp/SellerListActivity.kt
@@ -2,13 +2,48 @@ package com.mobile.lotterysmartapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.google.firebase.database.*
+import com.mobile.lotterysmartapp.model.User
 
 class SellerListActivity : AppCompatActivity() {
 
 
+    lateinit var ref: DatabaseReference
+    lateinit var sellerList: MutableList<User>
+    lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller_list)
+
+        sellerList = mutableListOf()
+        listView = findViewById(R.id.sellerList)
+        ref = FirebaseDatabase.getInstance().getReference("User")
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    sellerList.clear()
+                    for (s in snapshot.children) {
+                        val seller = s.getValue(User::class.java)
+                        if (seller != null) {
+                            sellerList.add(seller)
+                        }
+
+                        val adapter = SellerListAdapter(
+                            this@SellerListActivity,
+                            R.layout.seller_list,
+                            sellerList
+                        )
+                        listView.adapter = adapter
+                    }
+                }
+            }
+        })
     }
 }
