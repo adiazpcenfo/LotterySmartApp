fc96e2a64c614583c1525581792159100c718ffd
Josué Calderón <39226321+jcalderonv2@users.noreply.github.com>
2020-10-11 23:16:45 -0600

    Seller list

--- app/src/main/java/com/mobile/lotterysmartapp/SellerListActivity.kt
+++ app/src/main/java/com/mobile/lotterysmartapp/SellerListActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 import com.google.firebase.database.*
+import com.mobile.lotterysmartapp.model.Inventory
 import com.mobile.lotterysmartapp.model.User
 
 class SellerListActivity : AppCompatActivity() {
 
 
     lateinit var ref: DatabaseReference
-    lateinit var sellerList: MutableList<User>
+    lateinit var ref2: DatabaseReference
+    lateinit var sellerList: MutableList<Inventory>
+    lateinit var userList: MutableList<User>
     lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,15 @@ class SellerListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_seller_list)
 
         sellerList = mutableListOf()
+        userList = mutableListOf()
         listView = findViewById(R.id.sellerList)
-        ref = FirebaseDatabase.getInstance().getReference("User")
+        ref = FirebaseDatabase.getInstance().getReference("Inventory")
+        ref2 = FirebaseDatabase.getInstance().getReference("User")
+        loadTable()
+
+    }
+
+    fun loadTable() {
 
         ref.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(error: DatabaseError) {
@@ -30,9 +40,9 @@ class SellerListActivity : AppCompatActivity() {
                 if (snapshot.exists()) {
                     sellerList.clear()
                     for (s in snapshot.children) {
-                        val seller = s.getValue(User::class.java)
-                        if (seller != null) {
-                            sellerList.add(seller)
+                        val inventory = s.getValue(Inventory::class.java)
+                        if (inventory != null) {
+                            sellerList.add(inventory)
                         }
 
                         val adapter = SellerListAdapter(
--- app/src/main/java/com/mobile/lotterysmartapp/SellerListAdapter.kt
+++ app/src/main/java/com/mobile/lotterysmartapp/SellerListAdapter.kt
@@ -6,26 +6,55 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import com.google.firebase.database.*
+import com.mobile.lotterysmartapp.model.Inventory
 import com.mobile.lotterysmartapp.model.User
 
-class SellerListAdapter(val mCtx: Context, val layoutId: Int, val sellerList: List<User>) :
-    ArrayAdapter<User>(mCtx, layoutId, sellerList) {
+class SellerListAdapter(
+    val mCtx: Context,
+    val layoutId: Int,
+    val sellerList: MutableList<Inventory>
+) :
+    ArrayAdapter<Inventory>(mCtx, layoutId, sellerList) {
+
+    lateinit var ref: DatabaseReference
+    lateinit var userList: MutableList<User>
+
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
         val view: View = layoutInflater.inflate(layoutId, null)
+
+        userList = mutableListOf()
+
 
         val name = view.findViewById<TextView>(R.id.nameTextView)
         val email = view.findViewById<TextView>(R.id.emailTextView)
 
         val seller = sellerList[position]
 
-        name.text = seller.name
-        email.text = seller.email
+        ref = FirebaseDatabase.getInstance().getReference("User")
 
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    userList.clear()
+                    for (s in snapshot.children) {
+                        val user = s.getValue(User::class.java)
+                        if (user != null && seller.userEmail == user.email) {
+                            userList.add(user)
+
+                            name.text = user.name
+                            email.text = seller.userEmail
+                        }
+                    }
+                }
+            }
+        })
         return view
     }
-
-
 }
