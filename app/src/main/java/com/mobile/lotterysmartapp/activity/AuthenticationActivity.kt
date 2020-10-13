b3182c6c1ac4c6fe775bf2eb20a71940c4512cc7
Fran0697 <fcardenasmadrigal@gmail.com>
2020-10-13 00:43:13 -0600

    Save user credential
    -Implement save user credentials on a file so the user does not have to login every time he/she access the app from closed
    - Add Google login button. Implementation pending

--- app/src/main/java/com/mobile/lotterysmartapp/activity/AuthenticationActivity.kt
+++ app/src/main/java/com/mobile/lotterysmartapp/activity/AuthenticationActivity.kt
@@ -1,7 +1,9 @@
 package com.mobile.lotterysmartapp.activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
@@ -29,7 +31,7 @@ class AuthenticationActivity : AppCompatActivity() {
      *
      * @author Franklin Cardenas
      */
-    override fun onCreate(savedInstanceState : Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 
@@ -39,26 +41,51 @@ class AuthenticationActivity : AppCompatActivity() {
         bundle.putString("Message", "Application started")
         analytics.logEvent("InitScreen", bundle)
 
-        //Setup login page
         setup()
+
+        session()
     }
 
     /**
-     * Setup process for login
+     * Makes authentication layout visible.
+     *
+     * @author Franklin Cardenas
+     */
+    override fun onStart() {
+        super.onStart()
+        authenticationLayout.visibility = View.VISIBLE
+    }
+
+    /**
+     * Setup process for authorization activity
      *
      * @author Franklin Cardenas
      */
     private fun setup() {
         this.title = "Login"
+        setupRegisterButton()
+        setupLoginButton()
+        setupForgetPasswordButton()
+    }
 
-        //Setup register button
-        buttonRegister.setOnClickListener{
-            val intent = Intent(this,RegisterUserActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
+    /**
+     * Setup for Forget My Password Button.
+     *
+     * @author Franklin Cardenas
+     */
+    private fun setupForgetPasswordButton() {
+        buttonForgetPassword.setOnClickListener {
+            val forgetPasswordIntent = Intent(this, ForgetPasswordActivity::class.java).apply {}
+            startActivity(forgetPasswordIntent)
         }
+    }
 
-        //Setup Log In Button
+    /**
+     * Setup for Login Button.
+     *
+     * @author Franklin Cardenas
+     */
+    private fun setupLoginButton() {
         buttonLogIn.setOnClickListener {
             if (!editTextEmail.text.isNullOrBlank() && !editTextPassword.text.isNullOrBlank()) {
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(
@@ -70,31 +97,60 @@ class AuthenticationActivity : AppCompatActivity() {
                     } else {
                         alertUtil.simpleAlert(error, couldNotLogin, this)
                     }
+                }.addOnFailureListener {
+                    alertUtil.simpleAlert(error, it.message, this)
                 }
             } else {
                 alertUtil.simpleAlert(error, userAndPasswordNotPresent, this)
             }
         }
-        //Setup Forget Password Button
-        buttonForgetPassword.setOnClickListener {
-            val forgetPasswordIntent = Intent(this, ForgetPasswordActivity::class.java).apply {}
-            startActivity(forgetPasswordIntent)
+    }
+
+    /**
+     * Setup for Register Button.
+     *
+     * @author Jimena Vega
+     */
+    private fun setupRegisterButton() {
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegisterUserActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 
     /**
      * Show home screen and pass it the user credentials.
      *
-     * @author Franklin Cardenas
      * @param email user email
      * @param provider Account provider
+     * @author Franklin Cardenas
      */
-    private fun showHome(email : String?, provider : Provider) {
+    private fun showHome(email: String?, provider: Provider) {
         val homeIntent = Intent(this, HomeActivity::class.java).apply {
             putExtra(Constants.EMAIL, email)
-            putExtra(Constants.PROVIDER, provider)
+            putExtra(Constants.PROVIDER, provider.toString())
         }
         startActivity(homeIntent)
+    }
+
+
+    /**
+     * Checks if a session is active. If it is active, redirects the user to home page
+     *
+     * @author Franklin Cardenas
+     */
+    private fun session() {
+        val preferences =
+            getSharedPreferences(getString(R.string.preferences_file), Context.MODE_PRIVATE)
+        val email = preferences.getString(Constants.EMAIL, null)
+        val provider = preferences.getString(Constants.PROVIDER, null)
+
+        if (email != null && provider != null) {
+            authenticationLayout.visibility = View.INVISIBLE
+            showHome(email, Provider.valueOf(provider))
+        }
+
     }
 
 //    private fun showDrawMantain(email : String?, provider : Provider) {
--- app/src/main/java/com/mobile/lotterysmartapp/activity/HomeActivity.kt
+++ app/src/main/java/com/mobile/lotterysmartapp/activity/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.mobile.lotterysmartapp.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.mobile.lotterysmartapp.R
 import com.mobile.lotterysmartapp.model.Constants
 
@@ -25,20 +27,21 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
      *
      * @author Franklin Cardenas
      */
-    override fun onCreate(savedInstanceState : Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-
         //Get user and provider from login or register
-        //val bundle = intent.extras
-        //val email = bundle?.getString(Constants.EMAIL)
-        //val provider = bundle?.getString(Constants.PROVIDER)
+        val bundle = intent.extras
+        val email = bundle?.getString(Constants.EMAIL)
+        val provider = bundle?.getString(Constants.PROVIDER)
+
+        // Save email and password for the session
+        saveCredentials(email!!, provider!!)
 
         //Setup process for Home Activity
         setup()
-
     }
 
     /**
@@ -53,4 +56,37 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         TODO("Not yet implemented")
     }
+
+    /**
+     * Save user credentials inside a preference file.
+     *
+     * @param email - user email
+     * @param provider - authentication provider
+     * @author Franklin Cardenas
+     */
+    private fun saveCredentials(email: String, provider: String) {
+        val preferences =
+            getSharedPreferences(getString(R.string.preferences_file), Context.MODE_PRIVATE).edit()
+        preferences.putString(Constants.EMAIL, email)
+        preferences.putString(Constants.PROVIDER, provider)
+        preferences.apply()
+
+    }
+
+    /**
+     * Logout of the app.
+     * Delete user credentials from preferences file.
+     *
+     * @author Franklin Cardenas
+     */
+    private fun logout() {
+        val preferences =
+            getSharedPreferences(getString(R.string.preferences_file), Context.MODE_PRIVATE).edit()
+        preferences.clear()
+        preferences.apply()
+
+        FirebaseAuth.getInstance().signOut()
+        onBackPressed()
+    }
+
 }
